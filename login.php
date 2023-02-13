a768ba2406e4a6830adfd8398b50f00578fb5178
azaruddin <m.azaruddin@ilmux.com>
2023-02-13 22:38:46 +0530

    beatify

--- login.php
+++ login.php
@@ -32,24 +32,25 @@ else{
 <html lang="en">
 
 <head>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
-  </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
 
-  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-  <link rel="stylesheet" href="./styles/index.css" />
-  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-  <link rel="stylesheet" href="./styles/login.css" />
-  <script src="https://kit.fontawesome.com/7f5049b87a.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
-  <title>Document</title>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    <link rel="stylesheet" href="./styles/index.css" />
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <link rel="stylesheet" href="./styles/login.css" />
+    <script src="https://kit.fontawesome.com/7f5049b87a.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
+    <title>Document</title>
 </head>
+
 <body>
-    
+
     <?php include_once 'loginheader.php'?>
-        <?php
+    <?php
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     if($error){
     echo '
@@ -60,24 +61,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 ?>
     <div class="maindiv py-5">
-            <div class="login-box">
+        <div class="login-box">
 
-                <h2>Login</h2>
-                <form action="/HijabReadyToWear/login.php" method="post">
-                    <div class="container">
-                        <div class="input-div">
-                            <input type="text" placeholder=" Username" name="uname" required>
-                        </div>
-                        <div class="input-div">
-                            <input type="password" placeholder=" Password" name="password" required>
-                        </div>
-                        <button id="login-btn" type="submit">Login</button>
-                        <p class="signintext">Doesn't have an Account ? <a href="signup.php">Sign in</a></p>
+            <h2>Login</h2>
+            <form action="/HijabReadyToWear/login.php" method="post">
+                <div class="container">
+                    <div class="input-div">
+                        <input type="text" placeholder=" Username" name="uname" required>
                     </div>
+                    <div class="input-div">
+                        <input type="password" placeholder=" Password" name="password" required>
+                    </div>
+                    <button id="login-btn" type="submit">Login</button>
+                    <p class="signintext">Doesn't have an Account ? <a href="signup.php">Sign in</a></p>
+                </div>
 
-            </div>
         </div>
-<?php include 'footer.php'?>
+    </div>
+    <?php include 'footer.php'?>
 </body>
 
 </html>
--- signup.php
+++ signup.php
@@ -11,56 +11,59 @@ $sql ="INSERT INTO `users` (`username`, `phoneno`, `password`,`email`) VALUES ('
 $res= mysqli_query($conn,$usercheck);
 $num = mysqli_num_rows($res);
 if($username && $phoneno && $password && $email !== ""){
-    if($num >= 1){
-            echo '<div id="snackbar" class="bg-danger show" >user already exist</div>';
-    }
-    else{
- $result = mysqli_query($conn,$sql);
-      echo ' <div id="snackbar" class="bg-success show" >Account Created succesfully</div>';
-      header("location: login.php");
-    }
-  }
-else{
-    echo '<div id="snackbar" class="bg-danger show" >Missing Credentials</div>';
-}
-}
-?>
+    if($num >= 1){ echo '
+<div id="snackbar" class="bg-danger show">user already exist</div>
+'; } else{ $result = mysqli_query($conn,$sql); echo '
+<div id="snackbar" class="bg-success show">Account Created succesfully</div>
+'; header("location: login.php"); } } else{ echo '
+<div id="snackbar" class="bg-danger show">Missing Credentials</div>
+'; } } ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="styles/signup.css"/>
-</head>
-<body>  
+    <link rel="stylesheet" href="styles/signup.css" />
+  </head>
+  <body>
     <?php include_once 'loginheader.php'?>
     <div class="maindiv py-5">
       <div class="login-box">
-        <h2>Sign Up</h2>  
-            <form action="/HijabReadyToWear/signup.php" method="post" id="contactForm">
-                <div class="container">
-                    <div class="input-div">
-                        <input type="text" placeholder="Username" name="uname" >
-                    </div>
-                    <div class="input-div">
-                        <input type="number" placeholder="Phone no" name="phoneno" >
-                    </div>
-                    <div class="input-div">
-                        <input type="email" placeholder="Email" name="email" >
-                    </div>
-                    <div class="input-div">
-                        <input type="password" placeholder="Enter Password" name="password" >
-                    </div>
-                    <button id="login-btn" type="submit">Sign up</button>
-                    <p class="signintext">Already have an Account ? <a href="login.php">Log in</a></p>
-                </div>
-            </form>
-       </div>
+        <h2>Sign Up</h2>
+        <form
+          action="/HijabReadyToWear/signup.php"
+          method="post"
+          id="contactForm"
+        >
+          <div class="container">
+            <div class="input-div">
+              <input type="text" placeholder="Username" name="uname" />
+            </div>
+            <div class="input-div">
+              <input type="number" placeholder="Phone no" name="phoneno" />
+            </div>
+            <div class="input-div">
+              <input type="email" placeholder="Email" name="email" />
+            </div>
+            <div class="input-div">
+              <input
+                type="password"
+                placeholder="Enter Password"
+                name="password"
+              />
+            </div>
+            <button id="login-btn" type="submit">Sign up</button>
+            <p class="signintext">
+              Already have an Account ? <a href="login.php">Log in</a>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
 
-<?php include_once "footer.php" ?>
-</body>
+    <?php include_once "footer.php" ?>
+  </body>
 </html>
